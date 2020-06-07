d412c6492dc77cf889fb8df9b92a8cabb2d6d4ea
Vinoth Kumar <vnoitkumar@gmail.com>
2020-06-07 14:26:07 +0530

    Dynamic Themeing completed

---
+++ src/components/display/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import ModelWrapper from '../model-wrapper';
+import Background from './Background';
+
+function Display() {
+  return (
+    <ModelWrapper>
+      <Background />
+    </ModelWrapper>
+  );
+}
+
+export default Display;
--- src/context/theme-context.js
+++ src/context/theme-context.js
@@ -1,9 +1,17 @@
 import { createContext } from 'react';
+
 import { themes } from '../configs';
 
 const defaultValue = {
   theme: themes.default,
   changeTheme: () => {}
 };
+
+export function getTheme(themeName) {
+  if (themes.hasOwnProperty(themeName)) {
+    return themes[themeName];
+  }
+  return themes.default;
+}
 
 export default createContext(defaultValue);
